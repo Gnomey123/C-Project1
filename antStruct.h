5c3deac6ea43dc48f5755aeb63c0542dee1f9ff6
Gnomey123 <noamtuvia@gmail.com>
2021-04-25 12:29:24 -0400

    Some improvements with scanning for CW (untested)

--- antStruct.h
+++ antStruct.h
@@ -5,8 +5,8 @@ struct Ant {
     int mem_x;  //  positions of the memory when called
     int mem_y;
     int energy;
-    int itchUp;     //  boolean, specific itch for open space above
-    int itchDown;   //  boolean, specific itch for open space downward
-    int itchLeft;   //  boolean, specific itch for open space to the left
-    int itchRight;  //  boolean, specific itch for open space to the right
+    int itchUp;     //  (0: no itch, >0: itch w/ spaces avalible) itch for open space above
+    int itchDown;   //  (0: no itch, >0: itch w/ spaces avalible) itch for open space downward
+    int itchLeft;   //  (0: no itch, >0: itch w/ spaces avalible) itch for open space to the left
+    int itchRight;  //  (0: no itch, >0: itch w/ spaces avalible) itch for open space to the right
 } Michael;
--- mazeHelper.c
+++ mazeHelper.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "mazeHelper.h"
 #include "config.h"
+#include "antStruct.h"
 #define LENGTH 256
 
 FILE *mazeFile;
@@ -67,5 +68,148 @@ void printMatrix()
 		printf("\n");
 	}
 }
+
+void placeMarker(int x, int y)
+{
+	maze[x][y] = '?';
+}
+
+int spacesFree(int x, int y, int direction) // direction key: 1=Up, 2=Right, 3=Down, 4=Left
+{
+	int spaces = 0;
+	switch (direction)
+	{
+		case 1:
+			while ((maze[x-1][y] == '*') || (maze[x-1][y] == '$') || (maze[x-1][y] == '@'))
+			{
+				spaces++;
+				x--;
+			}
+			break;
+		case 2:
+			while ((maze[x][y+1] == '*') || (maze[x][y+1] == '$') || (maze[x][y+1] == '@'))
+			{
+				spaces++;
+				y++;
+			}
+			break;
+		case 3:
+			while ((maze[x+1][y] == '*') || (maze[x+1][y] == '$') || (maze[x+1][y] == '@'))
+			{
+				spaces++;
+				x++;
+			}
+			break;
+		case 4:
+			while ((maze[x][y-1] == '*') || (maze[x][y-1] == '$') || (maze[x][y-1] == '@'))
+			{
+				spaces++;
+				y--;
+			}
+			break;
+		default:
+			printf("\n[ERROR] illegal spacesFree direction called\n");
+			break;
+	}
+	return spaces;
+} 
+
+void CW_R(int x, int y)
+{
+	char charPosition = maze[x][y];
+	
+	printf("DEBUG: char=%c\n", charPosition);
+	
+	
+	switch (charPosition)
+	{
+		case '*':
+			Michael.itchRight = spacesFree(x, y, 2);
+			break;
+		case '@':
+			Michael.itchRight = spacesFree(x, y, 2);
+			break;
+		case '$':
+			Michael.itchRight = spacesFree(x, y, 2);
+			break;
+		default:
+			printf("\n[WARNING] CW called at illegal location: (%d|%d)\n");
+			break;
+	}
+}
+
+void CW_L(int x, int y)
+{
+	char charPosition = maze[x][y];
+	
+	printf("DEBUG: char=%c\n", charPosition);
+	
+	
+	switch (charPosition)
+	{
+		case '*':
+			Michael.itchLeft = spacesFree(x, y, 4);
+			break;
+		case '@':
+			Michael.itchLeft = spacesFree(x, y, 4);
+			break;
+		case '$':
+			Michael.itchLeft = spacesFree(x, y, 4);
+			break;
+		default:
+			printf("\n[WARNING] CW called at illegal location: (%d|%d)\n");
+			break;
+	}
+}
+
+void CW_U(int x, int y)
+{
+	char charPosition = maze[x][y];
+	
+	printf("DEBUG: char=%c\n", charPosition);
+	
+	
+	switch (charPosition)
+	{
+		case '*':
+			Michael.itchUp = spacesFree(x, y, 1);
+			break;
+		case '@':
+			Michael.itchUp = spacesFree(x, y, 1);
+			break;
+		case '$':
+			Michael.itchUp = spacesFree(x, y, 1);
+			break;
+		default:
+			printf("\n[WARNING] CW called at illegal location: (%d|%d)\n");
+			break;
+	}
+}
+
+void CW_D(int x, int y)
+{
+	char charPosition = maze[x][y];
+	
+	printf("DEBUG: char=%c\n", charPosition);
+	
+	
+	switch (charPosition)
+	{
+		case '*':
+			Michael.itchDown = spacesFree(x, y, 3);
+			break;
+		case '@':
+			Michael.itchDown = spacesFree(x, y, 3);
+			break;
+		case '$':
+			Michael.itchDown = spacesFree(x, y, 3);
+			break;
+		default:
+			printf("\n[WARNING] CW called at illegal location: (%d|%d)\n");
+			break;
+	}
+}
+
+
 
 
